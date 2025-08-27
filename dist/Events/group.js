c544125edfd1f8f77f5fc7e943aa04d78d5ce2fb
HenriqueX-DB20 <henrixdb20@gmail.com>
2025-08-27 10:53:47 -0300

    fix erros

--- dist/Events/group.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = register;
-function register(bot) {
-    bot.on("group", ({ action, jid }) => {
-        console.log(`Evento De Grupo: ${action} Em ${jid}`);
-    });
-}
